389bf28e2c35ed6e4f561595d6a610dd514c6101
N Tamez <nathan.tamez@vonage.com>
2023-10-11 09:14:56 +0100

    chore: get nexmo-client sdk to load without errors
    - install @types/socket.io-client:1.4.32 in devDependencies
    - add "socket.io-client" to types array in `tsconfig.app.json`
    - add global polyfill, `polyfills.ts` did't work so used a script tag in `index.html`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import NexmoClient, { type Application } from 'nexmo-client';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-nexmo-client-test';
+  app: Application | null = null;
+  constructor() {
+    const client = new NexmoClient();
+    client.createSession("token")
+      .then((app) => {
+        this.app = app;
+        console.log('app', app);
+      });
+  }
 }
